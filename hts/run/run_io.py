72ed80844147343d4a7630df0cd9369c22af9fb2
elkeschaper <elke@inf.ethz.ch>
2015-08-07 18:03:59 +0200

    run_io.py: Include more data in Run instance serialisation

--- hts/run/run_io.py
+++ hts/run/run_io.py
@@ -7,6 +7,7 @@
 """
 
 import csv
+import itertools
 import logging
 import os
 import re
@@ -33,14 +34,30 @@ def serialize_run_for_r(run_data, delimiter = ","):
     ..ToDo: Update to fit current Run class.
     '''
 
+    plate_layout_container = run_data.plate_layout()
+    plate_layout = plate_layout_container.layout
+    layout_general_type = plate_layout_container.layout_general_type
+    sample_replicate_count = plate_layout_container.sample_replicate_count
 
-    plate_layout = run_data.plate_layout().layout
-    all_data = [["x3_plate_name", "xp1", "xp2", "x1", "x2", "x3", "y", "y_type", "type"]]
+    all_data = [["x3_plate_name", "xp1", "xp2", "x1", "x2", "x3", "y", "y_type", "sample", "sample_type", "sample_replicate"]]
+    # Iterate over plates
     for iPlate_index, iPlate in run_data.plates.items():
+        # Iterate over readouts in plates (raw and preprocessed)
         for iReadout_index, iReadout in iPlate.read_outs.items():
-            data = iReadout.data
-            for i_row, i_row_plate, i_data_row, i_layout_row in zip(range(1, len(data) + 1), iReadout.axes['x'], data, plate_layout):
-                for i_col, i_col_plate, i_data, i_layout in zip(range(1, len(i_data_row) + 1), iReadout.axes['y'], i_data_row, i_layout_row):
-                    all_data.append([iPlate.name, i_row_plate, i_col_plate, i_row, i_col, iPlate_index, i_data, iReadout_index, i_layout])
+            # Iterate over the x axis ("width") and the y axis ("height")
+            for i_row, i_col in itertools.product(range(iPlate.height), range(iPlate.width)):
+                all_data.append([iPlate.name,
+                                iReadout.axes['x'][i_col],
+                                iReadout.axes['y'][i_row],
+                                i_col + 1,
+                                i_row + 1,
+                                iPlate_index,
+                                iReadout.data[i_row][i_col],
+                                iReadout_index,
+                                plate_layout[i_row][i_col],
+                                layout_general_type[i_row][i_col],
+                                sample_replicate_count[i_row][i_col]])
+
+
 
     return "\n".join([delimiter.join([str(j) for j in i]) for i in all_data])
