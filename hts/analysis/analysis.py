7b955bfeeaa61ac5a332a1ac5f2db798ba95db73
elkeschaper <elke@inf.ethz.ch>
2015-09-04 08:23:31 +0200

    analysis.py: Adapt to updated Fit library that runs all model fitting

--- hts/analysis/analysis.py
+++ hts/analysis/analysis.py
@@ -1,29 +1,21 @@
 # (C) 2015 Elke Schaper
 
 """
-    :synopsis: ``analysis`` implementes all methods connected to the
+    :synopsis: ``analysis`` implements all methods connected to the
     analysys of a high throughput screening experiment
 
     .. moduleauthor:: Elke Schaper <elke.schaper@isb-sib.ch>
 """
 
-import itertools
 import logging
-import numpy as np
 import os
-import pickle
-import re
 import sys
+from fit.model import Model
+from fit.model_dpia import ModelDPIA
 
 from hts.readout import readout, readout_dict
 
 LOG = logging.getLogger(__name__)
-
-PATH = '/Users/elkeschaper/Downloads/'
-
-
-#import pdb; pdb.set_trace()
-
 
 def perform_analysis(methods, data, *args, **kwargs):
     local_methods = {getattr(sys.modules[__name__], method_name): methods[method_name] for method_name in methods.keys()}
@@ -48,7 +40,7 @@ def perform_prion_fitting(data, file = "tata", *args, **kwargs):
     ..todo:: Add methods
     """
 
-    from hts.analysis import analysis_prion_propagation_model as appm
+    import pdb; pdb.set_trace()
 
     if 'plate_layout' in kwargs:
         plate_layout = kwargs['plate_layout']
@@ -59,39 +51,43 @@ def perform_prion_fitting(data, file = "tata", *args, **kwargs):
     else:
         raise ValueError('dpia_dilution is not in kwargs. Check e.g. definitions in the run config file.')
 
+    try:
+        path = kwargs.pop("analysis_result_path")
+        LOG.warning("analysis_result_path is not defined. Results are saved to the default path defined in the Fit library.")
+    except:
+        path = ""
+
+    model_name = "dPIA"
+
     results = {}
 
+    #For each run, several dPIA experiments are usually defined. Iterate through all dPIA experiments.
     for dpia_experiment_tag, dpia_plates in dpia_dilution.items():
+        # For each dpia_experiment_tag, perform the dPIA fitting for all defined dilutions (which all contain "neg" and "s" data).
         LOG.info("dpia_experiment_tag: {}, dpia_plates: {}".format(dpia_experiment_tag, dpia_plates))
-        dilutions = [float(i) for i in dpia_plates.values()]
-        LOG.info(dilutions)
-        all_thresholds=np.zeros([100,len(dilutions)]) # Legacy code - clean up
-        all_frac_below=np.zeros([100,len(dilutions)]) # Legacy code - clean up
-        #for dpia_tag, dpia_dilution in dpia_plates.items():
-        for i_dilution, dpia_dilution in enumerate(dilutions):
-            dpia_tag = next((dpia_tag for dpia_tag, i_dpia_dilution in dpia_plates.items() if float(i_dpia_dilution) == dpia_dilution), None)
-            LOG.info("dpia_tag: {};  dpia_dilution: {}".format(dpia_tag, dpia_dilution))
-            sample_y = data.read_outs[dpia_tag].filter_wells("s_0")
-            neg_control_y = data.read_outs[dpia_tag].filter_wells("neg")
-            # Legacy code - clean up:
-            temp=appm.neg_control_normaliser(sample_y,neg_control_y) # returns a vector of the fraction of points below the threshold, as a function of the threshold, units are in terms of standard deviations of the negative control, zeroed to the mean of the negtive control.
-            all_thresholds[:,i_dilution]=temp[:,0] # Legacy code - clean up
-            all_frac_below[:,i_dilution]=temp[:,1] # Legacy code - clean up
 
-        parameter_names, identifiers_all, parameters_all = appm.prion_fitter_input_parameters(dilutions_list=dilutions)
-        LOG.debug(parameter_names)
-        LOG.debug(identifiers_all)
-        LOG.debug(parameters_all)
-        LOG.debug(all_thresholds)
-        LOG.debug(all_frac_below)
+        # Prepare the fit input
+        # 1) run_data
+        run_data = {plate_name: {"value": dilution} for plate_name, dilution in dpia_plates.items()}
+        model_parameters = {"d": {"type": "local", "runs": run_data}}
+        LOG.info(model_parameters)
 
-        parameters_fit, fitter_output, upper_errors,lower_errors= appm.fit_prion_model(all_thresholds, all_frac_below, parameters_all, identifiers_all, parameter_names, appm.prions_fbelow_sat, basinhops=1)
+        # 2) model_data. model_data needs to follows the following form:
+        # {"runA": {"neg": np.array([1,2,1]), "s": np.array([3,4,4,3])}, "runB": {"neg": np.array([3,2,3]), "s": np.array([4,5,6,2])}, ...}
+        model_data = {plate_name: {"neg": data.read_outs[plate_name].filter_wells("neg"), "s": data.read_outs[plate_name].filter_wells("s")} for plate_name in dpia_plates.keys()}
+        LOG.info(model_data)
+
+        my_model = Model.create(origin="dict", model_class=ModelDPIA, model_name=model_name, data=model_data, parameters=model_parameters)
+        parameters_fit, fit = my_model.fit_model()
 
         n_propagons_per_well_undiluted_sample = parameters_fit[0,0]
-        sum_squares_error = fitter_output['fun']
-        plot_header = dpia_experiment_tag +'\nError(sum of squares) = %(err).1e, \nNumber of propagons per well in an undiluted sample = %(pri).1e' % {'err':sum_squares_error,'pri':n_propagons_per_well_undiluted_sample}
-        appm.plot_prion_propagation_model_fit(appm.prions_fbelow_sat, parameters_fit, all_frac_below, all_thresholds, plot_header, dilutions, tag = dpia_experiment_tag)
+        sum_squares_error = fit['fun']
+        plot_header = dpia_experiment_tag +'\nError(sum of squares) = %(err).1e, \nn_propagons per well (undiluted)  = %(pri).1e' % {'err':sum_squares_error,'pri':n_propagons_per_well_undiluted_sample}
+
+        my_model.plot_model_fit(parameters_fit, title=plot_header, path=path, file_trunk=dpia_experiment_tag)
+
+        # Define the return values
         results[dpia_experiment_tag] = {"sum_squares_error": sum_squares_error, "n_propagons_per_well_undiluted_sample": n_propagons_per_well_undiluted_sample}
 
-    import pdb; pdb.set_trace()
     return results
+
