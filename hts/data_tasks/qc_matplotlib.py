6a31030268f7cc3ce04c817fc780a952ae2a642d
elkeschaper <elke@inf.ethz.ch>
2016-01-26 12:39:32 +0100

    See #6, qc_matplotlib.py: Add plotting functions for GP data for batches and single plates

--- hts/data_tasks/qc_matplotlib.py
+++ hts/data_tasks/qc_matplotlib.py
@@ -8,9 +8,9 @@
     .. moduleauthor:: Elke Schaper <elke.schaper@isb-sib.ch>
 """
 
+import itertools
 import logging
 import math
-
 import matplotlib
 
 matplotlib.use('pdf')
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.axes_grid1 import AxesGrid
 import numpy as np
+
+import GPy
 
 
 LOG = logging.getLogger(__name__)
@@ -68,6 +70,57 @@ def heat_map_single(run, data_tag,  result_file=None, *args, **kwargs):
     fig.clear()
 
     return ax
+
+
+
+
+def heat_map_single_gaussian_process_model(run, data_tag_readout, sample_key, plate_tag, magnification=5, *args, **kwargs):
+    """ Create a heat_map for multiple readouts
+
+    Create a heat_map for multiple readouts
+
+
+model_as_gaussian_process(self, data_tag_readout, sample_key,
+                                  kernel_type='m32',
+                                  n_max_iterations=1000,
+                                  plot_kwargs=False,
+
+    """
+
+    wells_high_resolution = list(itertools.product(np.arange(0,run.width,1/magnification), np.arange(0,run.height,1/magnification)))
+    x_wells = np.array(wells_high_resolution)
+
+
+    plate = run.plates[plate_tag]
+    m = plate.model_as_gaussian_process(data_tag_readout=data_tag_readout, sample_key=sample_key, return_model=True, **kwargs)
+    y_mean, y_var  = m.predict(x_wells)
+    y_predicted_as_matrix = y_mean.reshape(magnification*run.width,magnification*run.height).transpose()
+
+    fig = plt.figure()
+
+    # im is of type matplotlib.image.AxesImage
+    plt.imshow(y_predicted_as_matrix)
+    plt.colorbar()
+
+    fig.set_size_inches(5, 4, forward=True)
+    plt.show()
+
+
+def slice_single_gaussian_process_model(run, data_tag_readout, sample_key, plate_tag, slice=5, **kwargs):
+    """ Create a heat_map for multiple readouts
+
+    Create a heat_map for multiple readouts
+
+
+    # Currently: using Plotly.
+    # Perhaps, matplotlib.axes._subplots.AxesSubplot can be integrated with Matplotlib's AxesGrid as above.
+    """
+
+    plate = run.plates[plate_tag]
+    m = plate.model_as_gaussian_process(data_tag_readout=data_tag_readout, sample_key=sample_key, return_model=True, **kwargs)
+
+    m.plot(fixed_inputs=[(1,slice)], plot_data=False)
+
 
 
 ################ Batch wise methods ####################
@@ -125,3 +178,111 @@ def heat_map_multiple(run, data_tag, result_file=None, n_plates_max=10, *args, *
 
     # Do we need this line?
     #fig.clear()
+
+
+
+
+def heat_map_multiple_gaussian_process_model(run, data_tag_readout, sample_key, result_file=None, magnification=5, n_plates_max=10, *args, **kwargs):
+    """ Create a heat_map for multiple readouts
+
+    Create a heat_map for multiple readouts
+
+
+model_as_gaussian_process(self, data_tag_readout, sample_key,
+                                  kernel_type='m32',
+                                  n_max_iterations=1000,
+                                  plot_kwargs=False,
+
+    """
+
+    wells_high_resolution = list(itertools.product(np.arange(0,run.width,1/magnification), np.arange(0,run.height,1/magnification)))
+    x_wells = np.array(wells_high_resolution)
+
+    plate_tags = run.plates.keys()
+    if len(plate_tags) > n_plates_max:
+        plate_tags = plate_tags[::round(len(plate_tags)/n_plates_max)]
+
+    a = math.ceil(len(plate_tags)/2)
+    b = 2
+
+    data = []
+    for plate_tag in plate_tags:
+        plate = run.plates[plate_tag]
+        m = plate.model_as_gaussian_process(data_tag_readout=data_tag_readout, sample_key=sample_key, return_model=True, **kwargs)
+        y_mean, y_var  = m.predict(x_wells)
+        y_predicted_as_matrix = y_mean.reshape(magnification*run.width,magnification*run.height).transpose()
+        data.append(y_predicted_as_matrix)
+
+
+
+    fig = plt.figure()
+
+    grid = AxesGrid(fig, 111,
+                    nrows_ncols=(b, a),
+                    axes_pad=0.05,
+                    share_all=True,
+                    label_mode="L",
+                    cbar_location="right",
+                    cbar_mode="single",
+                    )
+
+    for val, ax in zip(data,grid):
+        im = ax.imshow(val)
+        # im is of type matplotlib.image.AxesImage
+
+
+    grid.cbar_axes[0].colorbar(im)
+
+    for cax in grid.cbar_axes:
+        cax.toggle_label(False)
+
+    if result_file:
+        pp = PdfPages(result_file)
+        pp.savefig(fig, dpi=20, bbox_inches='tight')
+        pp.close()
+
+    fig.set_size_inches(30, 14, forward=True)
+    plt.show()
+
+
+
+
+def slice_multiple_gaussian_process_model(run, data_tag_readout, sample_key, result_file=None, slice=5, n_plates_max=10, *args, **kwargs):
+    """ Create a heat_map for multiple readouts
+
+    Create a heat_map for multiple readouts
+
+
+    # Currently: using Plotly.
+    # Perhaps, matplotlib.axes._subplots.AxesSubplot can be integrated with Matplotlib's AxesGrid as above.
+    """
+
+
+
+    plate_tags = run.plates.keys()
+    if len(plate_tags) > n_plates_max:
+        plate_tags = plate_tags[::round(len(plate_tags)/n_plates_max)]
+
+    a = math.ceil(len(plate_tags)/2)
+    b = 2
+
+    models = []
+    for plate_tag in plate_tags:
+        plate = run.plates[plate_tag]
+        m = plate.model_as_gaussian_process(data_tag_readout=data_tag_readout, sample_key=sample_key, return_model=True, **kwargs)
+        models.append(m)
+
+    GPy.plotting.change_plotting_library('plotly')
+    figure = GPy.plotting.plotting_library().figure(len(models), 1, shared_xaxes=True,)
+
+    for i,m in enumerate(models):
+        canvas = m.plot(figure=figure, fixed_inputs=[(1,slice)], row=(i+1), plot_data=False)
+        # canvas is of type matplotlib.axes._subplots.AxesSubplot
+
+    #grid.cbar_axes[0].colorbar(im)
+
+    GPy.plotting.show(canvas, filename='basic_gp_regression_notebook_slicing')
+
+    GPy.plotting.change_plotting_library('matplotlib')
+
+
