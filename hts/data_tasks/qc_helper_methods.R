66803700ce4412079907cd958ec9512510b28672
elkeschaper <elke@inf.ethz.ch>
2016-06-01 10:06:51 +0200

    qc_helper_methods.R: Update beautifier method to current ggplot2 versions; Add methods for robust linear regression

--- hts/data_tasks/qc_helper_methods.R
+++ hts/data_tasks/qc_helper_methods.R
@@ -1,4 +1,4 @@
-library(ggplot2); library(scales); library(grid); library(RColorBrewer); library("plyr")
+library(ggplot2); library(scales); library(grid); library(RColorBrewer); library("plyr"); library(MASS)
 
 # Pretty, but not perfect in case of red-green blindness:
 #colors = c("darkblue", "blue", "turquoise", "green", "yellow", "red", "darkred")
@@ -41,6 +41,21 @@ return(p)
 }
 
 
+tile_plot_x1x2x3_mark_conditionally <- function(data, column, save, condition, color){
+    # Inspired by: # http://stackoverflow.com/questions/13258454/marking-specific-tiles-in-geom-tile-geom-raster/13260999#13260999
+    data$x2 = factor(data$x2, levels = max(data$x2):min(data$x2))
+    data$x1 = factor(data$x1, levels = min(data$x1):max(data$x1))
+    p = ggplot(data, aes(x= x1,y=x2, fill=y), environment = environment()) + facet_wrap(~x3)
+    p = p + geom_raster() + scale_fill_gradientn(colours=colors, name ="y")
+    p = p + coord_fixed(ratio=1)
+    p = p + beautifier()
+    p = p + geom_point(data=subset(data, eval(parse(text=condition))), aes(x=x1,y=x2), size=0.4, color=color, shape=4)
+    return(p)
+}
+
+
+
+
 get_label_positions <- function(x, thresholds){
   
   spread = diff(range(x))
@@ -48,47 +63,6 @@ get_label_positions <- function(x, thresholds){
   label_positions[[1]] = min(thresholds[[1]] - spread/10, min(x) + spread/5)
   label_positions[[length(label_positions)]] = max(thresholds[[length(thresholds)]] + spread/10, min(x) - spread/5)
   return(label_positions)
-}
-
-
-
-fte_theme <- function() {
-
-# Generate the colors for the chart procedurally with RColorBrewer
-palette <- brewer.pal("Greys", n=9)
-color.background = palette[2]
-color.grid.major = palette[3]
-color.axis.text = palette[6]
-color.axis.title = palette[7]
-color.title = palette[9]
-
-# Begin construction of chart
-theme_bw(base_size=9) +
-
-# Set the entire chart region to a light gray color
-theme(panel.background=element_rect(fill=color.background, color=color.background)) +
-theme(plot.background=element_rect(fill=color.background, color=color.background)) +
-theme(panel.border=element_rect(color=color.background)) +
-
-# Format the grid
-theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
-theme(panel.grid.minor=element_blank()) +
-theme(axis.ticks=element_blank()) +
-
-# Format the legend, but hide by default
-theme(legend.position="none") +
-theme(legend.background = element_rect(fill=color.background)) +
-theme(legend.text = element_text(size=7,color=color.axis.title)) +
-
-# Set title and axis labels, and format these and tick marks
-theme(plot.title=element_text(color=color.title, size=10, vjust=1.25)) +
-theme(axis.text.x=element_text(size=7,color=color.axis.text)) +
-theme(axis.text.y=element_text(size=7,color=color.axis.text)) +
-theme(axis.title.x=element_text(size=8,color=color.axis.title, vjust=0)) +
-theme(axis.title.y=element_text(size=8,color=color.axis.title, vjust=1.25)) +
-
-# Plot margins
-theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm"))
 }
 
 
@@ -117,8 +91,8 @@ theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
 theme(panel.grid.minor=element_blank()) +
 theme(axis.ticks=element_blank()) +
 
-# Format the legend, but hide by default
-theme(legend.position="none") +
+# Format the legend.
+# theme(legend.position="none") # Hiding the legend.
 theme(legend.background = element_rect(fill=color.overlaid_background)) +
 theme(legend.text=element_text(family="sans",size=10,face='italic',hjust=0,color=color.axis.title)) +
 theme(legend.key=element_rect(fill=color.overlaid_background,color=color.overlaid_background)) +
@@ -140,7 +114,6 @@ theme(plot.margin = unit(c(0.35,0.2,0.3,0.35), "cm"))
 }
 
 
-
 # http://stackoverflow.com/questions/7549694/ggplot2-adding-regression-line-equation-and-r2-on-graph
 lm_eqn = function(m) {
 
@@ -152,6 +125,42 @@ lm_eqn = function(m) {
     eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
   } else {
     eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2,l)
+  }
+
+  as.character(as.expression(eq));
+}
+
+
+# http://stats.stackexchange.com/questions/83826/is-a-weighted-r2-in-robust-linear-model-meaningful-for-goodness-of-fit-analys
+r2ww <- function(x){
+  SSe <- sum((x$w*x$resid)^2); #the residual sum of squares is weighted
+  observed <- x$resid+x$fitted;
+  # SSt <- sum((x$w*(observed-mean(observed)))^2); #the total sum of squares is weighted
+  SSt <- sum((x$w*observed-mean(x$w*observed))^2)
+  value <- 1-SSe/SSt;
+  return(value);
+}
+
+r2 <- function(x){
+  SSe <- sum((x$resid)^2);
+  observed <- x$resid+x$fitted;
+  SSt <- sum((observed-mean(observed))^2);
+  value <- 1-SSe/SSt;
+  return(value);
+}
+
+# Convert a robust rlm model to a string with linear model formula, r2 and r2ww
+rlm_eqn = function(m) {
+
+  l <- list(a = format(coef(m)[1], digits = 2),
+      b = format(abs(coef(m)[2]), digits = 2),
+      r2 = format(r2(m), digits = 3),
+      r2ww = format(r2ww(m), digits = 3));
+
+  if (coef(m)[2] >= 0)  {
+    eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2~","~~italic(r)^2~ww~"="~r2ww,l)
+  } else {
+    eq <- substitute(italic(y) == a - b %.% italic(x)*","~~italic(r)^2~"="~r2~","~~italic(r)^2~ww~"="~r2ww,l)
   }
 
   as.character(as.expression(eq));
