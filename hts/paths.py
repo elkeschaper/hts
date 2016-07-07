94122920d598a852aa651f5eb3bb0011322efd65
elkeschaper <elke@inf.ethz.ch>
2016-07-07 09:40:38 +0200

    paths.py: Add path for tutorials

--- hts/paths.py
+++ hts/paths.py
@@ -13,3 +13,4 @@ PACKAGE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "test_data")
 
 TUTORIAL_DATA_NORMALIZATION_PATH = os.path.join(DATA_DIRECTORY, "Runs", "run_config_tutorial_1.txt")
+TUTORIAL_QC_PATH = os.path.join(DATA_DIRECTORY, "Runs", "run_config_tutorial_1.txt")
